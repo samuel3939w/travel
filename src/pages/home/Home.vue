c937eb87d9f601d18c50cd42fad0e5058f91f26a
Sam <samuel3939w@gmail.com>
2021-06-23 15:17:24 +0800

    finish page

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from "./components/Icons.vue";
 import HomeRecommend from "./components/Recommend.vue";
 import HomeWeekend from "./components/Weekend.vue";
 import axios from "axios";
+import { mapState } from "vuex";
 
 export default {
   name: "Home",
@@ -27,29 +28,40 @@ export default {
   },
   data() {
     return {
+      lastCity: "",
       swiperList: [],
-      iconList:[],
-      recommendList:[],
-      weekendList:[]
+      iconList: [],
+      recommendList: [],
+      weekendList: [],
     };
+  },
+  computed: {
+    ...mapState(["city"]),
   },
   methods: {
     getHomeInfo() {
-      axios.get("/api/index.json").then(this.getHomeInfoSucc);
+      axios.get("/api/index.json?city=" + this.city).then(this.getHomeInfoSucc);
     },
     getHomeInfoSucc(res) {
       res = res.data;
       if (res.ret && res.data) {
         const data = res.data;
         this.swiperList = data.swiperList;
-        this.iconList=data.iconList;
-        this.recommendList=data.recommendList;
-        this.weekendList=data.weekendList;
+        this.iconList = data.iconList;
+        this.recommendList = data.recommendList;
+        this.weekendList = data.weekendList;
       }
     },
   },
   mounted() {
+    this.lastCity = this.city;
     this.getHomeInfo();
+  },
+  activated() {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city;
+      this.getHomeInfo();
+    }
   },
 };
 </script>
